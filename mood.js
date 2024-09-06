62e949e4cc3a1e2a4b712991f21933cb16d51e24
Bs label <163457658+Blackswan19@users.noreply.github.com>
2024-09-06 16:47:25 +0530

    Update mood.js

--- mood.js
+++ mood.js
@@ -33,13 +33,13 @@ window.addEventListener('load', function () {
             : '<i class="fa-solid fa-repeat"></i>';
     }
 
-    // When the song ends, check if repeat is on, and if so, replay the song, otherwise play a random song
+    // When the song ends, check if repeat is on, and if so, replay the song
     audioPlayer.addEventListener('ended', () => {
         if (isRepeat) {
             audioPlayer.currentTime = 0;
             audioPlayer.play();
         } else {
-            playRandomSong();
+            playNextSong();
         }
     });
 
@@ -124,17 +124,17 @@ window.addEventListener('load', function () {
                 if (playbackPosition > 0 && audioPlayer.src) {
                     playAudioFromPosition(audioPlayer.src, playbackPosition);
                 } else {
-                    playRandomSong();
+                    playNextSong();
                     hasInteracted = true;
                 }
             }
         }
     }
 
-    function playRandomSong() {
-        currentSongIndex = Math.floor(Math.random() * playButtons.length);
-        var randomSong = playButtons[currentSongIndex].parentElement.getAttribute('data-src');
-        playAudio(randomSong);
+    function playNextSong() {
+        currentSongIndex = (currentSongIndex + 1) % playButtons.length;
+        var nextSong = playButtons[currentSongIndex].parentElement.getAttribute('data-src');
+        playAudio(nextSong);
     }
 
     function playPreviousSong() {
@@ -215,8 +215,10 @@ window.addEventListener('load', function () {
     });
 
     nextBtn.addEventListener('click', function () {
-        playRandomSong();
+        playNextSong();
     });
+
+    audioPlayer.addEventListener('ended', playNextSong);
 
     volumeBar.addEventListener('input', function () {
         audioPlayer.volume = volumeBar.value / 100;
