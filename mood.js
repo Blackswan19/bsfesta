117e31515a354f4fd9f449a0dfc1d7919c141642
Bs label <163457658+Blackswan19@users.noreply.github.com>
2024-10-02 20:55:55 +0530

    Update mood.js

--- mood.js
+++ mood.js
@@ -284,3 +284,11 @@ document.addEventListener('DOMContentLoaded', function () {
         document.addEventListener('touchend', stopMove);
     });
 });
+
+function displayText(sectionId) {
+    var sections = document.getElementsByClassName("section");
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = "none";
+    }
+    document.getElementById(sectionId).style.display = "block";
+}
