f15d5610c850daca5e0f8b51180ff67d02d49b72
Bs label <163457658+Blackswan19@users.noreply.github.com>
2024-10-31 18:25:20 +0530

    Update newapper.js

--- newapper.js
+++ newapper.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Check if the current date is less than the expiration date
     if (currentDate <= expirationDate) {
         // Select only the specific song list items by their titles
-        const songTitles = ["Young And Beautiful","ELEVEN","That is What I Like","When I Was Your Man","Just the Way You Are","Wake Up in the Sky","Marry You","Locked out of Heaven","Beat It","The Lazy Song","All The Stars"];
+        const songTitles = ["Young And Beautiful","ELEVEN","That is What I Like","When I Was Your Man","Just the Way You Are","Wake Up in the Sky","Marry You","Locked out of Heaven","Beat It","The Lazy Song","All The Stars","24K Magic"];
         const songs = document.querySelectorAll("#song-list li");
         
         songs.forEach(song => {
