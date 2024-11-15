8661af2e5012ac6796441111f16a2db0c3b2f6de
Bs label <163457658+Blackswan19@users.noreply.github.com>
2024-11-16 00:04:03 +0530

    Update telugu.js

--- telugu.js
+++ telugu.js
@@ -16,6 +16,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
     pauseAudio();
 
+    document.addEventListener("DOMContentLoaded", () => {
+        const customMenu = document.querySelector(".custom-menu");
+
+        // Show custom menu on right-click
+        document.addEventListener("contextmenu", (event) => {
+            event.preventDefault();
+            customMenu.style.display = "block";
+            customMenu.style.top = `${event.pageY}px`;
+            customMenu.style.left = `${event.pageX}px`;
+        });
+
+        // Hide the menu when clicking elsewhere
+        document.addEventListener("click", () => {
+            customMenu.style.display = "none";
+        });
+    });
+
+
     audioPlayer.addEventListener('error', (e) => {
         console.error('Error with audio playback:', e);
     });
