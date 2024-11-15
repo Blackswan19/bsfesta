b23d044a5aee41184d527866a70be98ac9d634ba
Bs label <163457658+Blackswan19@users.noreply.github.com>
2024-11-15 23:59:50 +0530

    Update filter.js

--- filter.js
+++ filter.js
@@ -18,6 +18,22 @@ function displayText(sectionId) {
     }
     document.getElementById(sectionId).style.display = "block";
 }
+document.addEventListener("DOMContentLoaded", () => {
+        const customMenu = document.querySelector(".custom-menu");
+
+        // Show custom menu on right-click
+        document.addEventListener("contextmenu", (event) => {
+            event.preventDefault();
+            customMenu.style.display = "block";
+            customMenu.style.top = `${event.pageY}px`;
+            customMenu.style.left = `${event.pageX}px`;
+        });
+
+        // Hide the menu when clicking elsewhere
+        document.addEventListener("click", () => {
+            customMenu.style.display = "none";
+        });
+    });
 
 document.addEventListener('DOMContentLoaded', function () {
     var albumLinksContainer = document.getElementById('album-links');
